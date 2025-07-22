acfd5b6b4aeb8c29b2e24751e0aeb4c9dceebd8c
Pedro Condomi <pedrocondomi@gmail.com>
2025-07-22 20:59:54 -0300

    app.js ahora es index.js

--- index.js
+++ index.js
@@ -2,16 +2,11 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
-// TODO Subir a Github, Actualizar README, comentar todo
-// TODO crear usuarios y autorizacion JWT
-// TODO subir a Vercel (atento a las variables de entorno)
-
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public")); // Esto es al pedo por ahora
 
 app.get("/", (req, res) => {
   res.send("kepasaca");
@@ -25,5 +20,3 @@ app.use("/api", routes.usersRouter);
 app.listen(PORT, () => {
   console.log(`Escuchando en puerto ${PORT}`);
 });
-
-// Ideas para e-commerce: ideas.txt
